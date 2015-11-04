4d1c8ac7e12e6e539e5ed4d6cf0bf16f829cc59b
songzhw <songzhw2012@gmail.com>
2015-11-04 17:07:05 +0800

    add a Https note : CA cert and Hostname Verifier

--- app/src/main/kotlin/cn/six/payx/net/RetrofitSingleton.kt
+++ app/src/main/kotlin/cn/six/payx/net/RetrofitSingleton.kt
@@ -4,6 +4,9 @@ import retrofit.RestAdapter
 import retrofit.android.AndroidLog
 
 object RetrofitSingleton {
+	// default : use HttpUrlConnection(src: Platform.java # defaultClient() )
+	// so I still have to check the hostName and CA cert
+	// of course, a simpler way is using OkHttp as a client of Retrofit
     fun getNetService() : NetService{
         var restAdapter = RestAdapter.Builder()
                 .setEndpoint("https://xxx.xxx.xxx.com")
