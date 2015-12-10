35d004f5c7684ca308a1c7d0dd8a7fc286bf15f2
songzhw <songzhw2012@gmail.com>
2015-12-10 17:39:12 +0800

    RxJava : the limition in the input of a EditText

--- app/src/main/kotlin/cn/six/payx/ui/TestRxActivity.kt
+++ app/src/main/kotlin/cn/six/payx/ui/TestRxActivity.kt
@@ -6,8 +6,11 @@ import android.view.View
 import cn.six.payx.R
 import cn.six.payx.core.BaseActivity
 import cn.six.payx.presenter.TestRxPresenter
+import cn.six.payx.util.showToast
 import kotlinx.android.synthetic.activity_test_rx.*
 import rx.android.view.ViewObservable
+import rx.android.widget.OnTextChangeEvent
+import rx.android.widget.WidgetObservable
 import java.util.concurrent.TimeUnit
 import kotlin.properties.Delegates
 
@@ -30,6 +33,46 @@ public class TestRxActivity : BaseActivity(){
 
         // buffer click
         bufferClick()
+
+        // debounce search
+        debounceSearch()
+
+        // limition on the input
+        limitInput()
+
+    }
+
+    fun debounceSearch(){
+//        WidgetObservable.text(etSearch)
+    }
+
+    // 1. You can only input two decimal in this EditText
+    // 2. and EditText will add 0 in the first position if you input "." at first
+    fun limitInput(){
+        WidgetObservable.text(etInputLimit)
+            .map { it.text().toString() }
+            .subscribe {
+                if(it.startsWith(".")){
+                    etInputLimit.setText("0$it")
+                    etInputLimit.setSelection(it.length+1) //now has a extro "0", so +1
+                }
+            }
+
+        WidgetObservable.text(etInputLimit)
+            .map { it.text().toString() }
+            .filter{ str : String ->
+                var pointIndex = str.indexOf(".")
+                pointIndex >=0
+                    && str.substring(pointIndex).length > 3
+            }
+            .subscribe{ str : String ->
+                var pointIndex = str.indexOf(".")
+                if(pointIndex >= 0){
+                    var ret = str.subSequence(0, pointIndex+3).toString()
+                    etInputLimit.setText(ret)
+                    etInputLimit.setSelection(ret.length)
+                }
+            }
     }
 
     fun noStickyClick(){
@@ -44,6 +87,8 @@ public class TestRxActivity : BaseActivity(){
         // "szw click--subscriber" will be triggered every two seconds
         // even if the data is a empty list : "szw click--subscribe []"
         // this may be a bad choice
+
+        /*
         ViewObservable.clicks(btnBufferClick)
             .map{
                 println("szw click -- map()")
@@ -53,6 +98,9 @@ public class TestRxActivity : BaseActivity(){
             .subscribe{ data : MutableList<String> ->
                 println("szw click -- subscribe $data")
             }
+        */
+
+
         /*
 // 1. no click event. Still trigger subscribe{} every two seconds
 19:50.361 I/System.out: szw click -- subscribe []
