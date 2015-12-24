e89228fbb61f113da6635efd2cb96fa7b9538cd3
songzhw <songzhw2012@gmail.com>
2015-12-24 10:03:51 +0800

    test the Memory usage in Kotlin + Rxjava 1. BehaviorSubject should call "onCompleted()" after it is over 2. Subscription should be called "unsubscribed()"
    p.s : I still found there are some object in SplashActivity remains when the SplashActivity's instance is over.

--- app/src/main/kotlin/cn/six/payx/ui/SplashActivity.kt
+++ app/src/main/kotlin/cn/six/payx/ui/SplashActivity.kt
@@ -11,6 +11,7 @@ import cn.six.payx.util.showToast
 import com.nostra13.universalimageloader.core.ImageLoader
 
 import kotlinx.android.synthetic.activity_splash.*
+import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 import rx.subjects.BehaviorSubject
@@ -20,11 +21,11 @@ import kotlin.properties.Delegates
 
 public class SplashActivity : BaseActivity() {
     val jumpObservable : BehaviorSubject<Void> = BehaviorSubject.create()
+    var sub1 : Subscription? = null
+    var sub2 : Subscription? = null
     var isFinishedSplash = false
-
-    // show how to delegate a lazy member
     val hello : String by lazy{
-        "hello kotlin" // resource.getString(R.string.hello)
+        "str"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,7 +42,7 @@ public class SplashActivity : BaseActivity() {
         var req = BaseRequest(raw)
 
         var netService = RetrofitSingleton.getNetService()
-        netService.getSplashInfo_Post(req.msg,req.sign)
+        sub1 = netService.getSplashInfo_Post(req.msg,req.sign)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
@@ -49,12 +50,13 @@ public class SplashActivity : BaseActivity() {
                             var imageLoader = ImageLoader.getInstance()
                             imageLoader.displayImage(resp.splashUrl, iv_splash_ad)
                             jumpObservable.onNext(null) // go to the home page
+                            jumpObservable.onCompleted()
                          },
                         {error ->
                             showToast(error.getMessage().toString())
                         })
 
-        jumpObservable.asObservable()
+        sub2 = jumpObservable.asObservable()
             .delay(2, TimeUnit.SECONDS)
             .subscribe{
                 isFinishedSplash = true
@@ -62,5 +64,11 @@ public class SplashActivity : BaseActivity() {
                 this.finish()
             }
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sub1?.unsubscribe()
+        sub2?.unsubscribe()
     }
 }
